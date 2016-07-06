c8c0d630894a4cca83368df730324b9de38d25a8
sarnecky <ssarnecki34@gmail.com>
2016-07-06 14:41:02 +0200

    Dodanie home controlera i widoku

---
+++ zabawa z gitem/zabawa z gitem/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zabawa_z_gitem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //utowrzenie plikus
+            return View();
+        }
+    }
+}
