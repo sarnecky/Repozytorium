6f841dbab54283205b0ea4bef6e6cd449a256a47
sarnecky <ssarnecki34@gmail.com>
2016-07-06 15:18:42 +0200

    przywrocenie starej galezi do mastera

--- zabawa z gitem/zabawa z gitem/Controllers/HomeController.cs
+++ zabawa z gitem/zabawa z gitem/Controllers/HomeController.cs
@@ -14,7 +14,6 @@ namespace zabawa_z_gitem.Controllers
             //utowrzenie plikus
             return View();
             //jakas tam edycja
-            //zmiana w Nowej gałęzi
-        }   //znowu jakies zmiany
+        }
     }
 }
