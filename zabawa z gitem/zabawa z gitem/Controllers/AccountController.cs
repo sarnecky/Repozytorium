62b152364cde6e950ca2b8824ec1c65f22f8ebb1
sarnecky <ssarnecki34@gmail.com>
2016-07-16 17:58:28 +0200

    Dodanie widoku, modelu i controlera do rejstracji

---
+++ zabawa z gitem/zabawa z gitem/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using zabawa_z_gitem.Models;
+
+namespace zabawa_z_gitem.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            //walidacja po stronie serwera, na podstawie danych ktore przyszly z widoku(po binodowaniu)
+            //podwojna walidacja ze wzgledu na mozliwosc wylaczenia js po stronie klienta
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+       
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            //walidacja po stronie serwera, na podstawie danych ktore przyszly z widoku(po binodowaniu)
+            //podwojna walidacja ze wzgledu na mozliwosc wylaczenia js po stronie klienta
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ zabawa z gitem/zabawa z gitem/Models/AccountViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace zabawa_z_gitem.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Wprowadz email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Wprowadz hasło")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage = "Minimalna dlugosc to 6 znaków", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Hasla nie sa identyczne")]
+        public string ConfirmPassword { get; set; }
+    }
+}
