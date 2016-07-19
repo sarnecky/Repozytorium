eda3b0f1092a067b7f88b23b52a349af09de5c4d
sarnecky <ssarnecki34@gmail.com>
2016-07-19 14:48:38 +0200

    dodanie migracji, ze wzgledu na blad bazy danych

---
+++ zabawa z gitem/zabawa z gitem/Migrations/201607191246205_InitialMigrations.cs
@@ -0,0 +1,16 @@
+namespace zabawa_z_gitem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
